b46dbd4890a7f0eeb3708e45a0edc54787f47956
fotistsiou <fotis.tsioumas@outlook.com>
2025-01-21 08:43:14 +0200

    Step 1

--- src/step_1/Main.java
+++ src/step_1/Main.java
@@ -17,4 +17,15 @@ package step_1;
  */
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+            Starting to make a coffee
+            Grinding coffee beans
+            Boiling water
+            Mixing boiled water with crushed coffee beans
+            Pouring coffee into the cup
+            Pouring some milk into the cup
+            Coffee is ready!
+            """);
+    }
 }
