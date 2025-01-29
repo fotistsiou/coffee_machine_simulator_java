0091f92045326267a893a5de319d6289d2cf1345
fotistsiou <fotis.tsioumas@outlook.com>
2025-01-29 22:59:06 +0200

    Step 5

--- src/step_5/Main.java
+++ src/step_5/Main.java
@@ -1,5 +1,7 @@
 package step_5;
 
+import java.util.Scanner;
+
 /**
  * Keep track of the supplies
  * --------------------------
@@ -34,4 +36,99 @@ package step_5;
  */
 
 public class Main {
+    static final int waterEspresso = 250, beansEspresso = 16, costEspresso = 4;
+    static final int waterLatte = 350, milkLatte = 75, beansLatte = 20, costLatte = 7;
+    static final int waterCappuccino = 200, milkCappuccino = 100, beansCappuccino = 12, costCappuccino = 6;
+    static int waterStock = 400, milkStock = 540, beansStock = 120, cupsStock = 9, money = 550;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean theEnd = false;
+        while (!theEnd) {
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
+            switch (scanner.nextLine()) {
+                case "buy" -> Main.buy();
+                case "fill" -> Main.fill();
+                case "take" -> Main.take();
+                case "remaining" -> Main.printStock();
+                case "exit" -> theEnd = true;
+            }
+        }
+        scanner.close();
+    }
+
+    static void printStock() {
+        System.out.println("The coffee machine has:");
+        System.out.printf("%d ml of water%n", Main.waterStock);
+        System.out.printf("%d ml of milk%n", Main.milkStock);
+        System.out.printf("%d g of coffee beans%n", Main.beansStock);
+        System.out.printf("%d disposable cups%n", Main.cupsStock);
+        System.out.printf("$%d of money%n", Main.money);
+    }
+
+    static void buy() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        String choice = scanner.nextLine();
+        int waterRemove = 0, milkRemove = 0, beansRemove = 0, addMoney = 0;
+        switch (choice) {
+            case "1" -> {
+                waterRemove = waterEspresso;
+                beansRemove = beansEspresso;
+                addMoney += costEspresso;
+            }
+            case "2" -> {
+                waterRemove = waterLatte;
+                milkRemove = milkLatte;
+                beansRemove = beansLatte;
+                addMoney += costLatte;
+            }
+            case "3" -> {
+                waterRemove = waterCappuccino;
+                milkRemove = milkCappuccino;
+                beansRemove = beansCappuccino;
+                addMoney += costCappuccino;
+            }
+            case "back" -> {
+                return;
+            }
+        }
+        if (Main.waterStock < waterRemove) {
+            System.out.println("Sorry, not enough water!");
+            return;
+        }
+        if (Main.milkStock < milkRemove) {
+            System.out.println("Sorry, not enough milk!");
+            return;
+        }
+        if (Main.beansStock < beansRemove) {
+            System.out.println("Sorry, not enough beans!");
+            return;
+        }
+        if (Main.cupsStock <= 0) {
+            System.out.println("Sorry, not enough cups!");
+            return;
+        }
+        Main.waterStock -= waterRemove;
+        Main.milkStock -= milkRemove;
+        Main.beansStock -= beansRemove;
+        Main.cupsStock--;
+        Main.money += addMoney;
+        System.out.println("I have enough resources, making you a coffee!");
+    }
+
+    static void fill() {
+        System.out.println("Write how many ml of water you want to add:");
+        Main.waterStock += scanner.nextInt();
+        System.out.println("Write how many ml of milk you want to add:");
+        Main.milkStock += scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        Main.beansStock += scanner.nextInt();
+        System.out.println("Write how many disposable cups you want to add:");
+        Main.cupsStock += scanner.nextInt();
+    }
+
+    static void take() {
+        System.out.printf("I gave you $%d", Main.money);
+        Main.money = 0;
+    }
 }
